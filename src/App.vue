f194871c8274c6b09e1a849e83991281e4a2032d
Hninn Hninn <swaltaweain528@gmail.com>
2020-07-17 15:47:13 +0630

    add template and validate

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <navbar />
+    <section class="section">
+      <div class="container is-desktop">
+        <router-view />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+export default {
+  components: { Navbar }
+};
+</script>
---
+++ src/components/Register.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="columns">
+    <div class="column is-half is-offset-one-quarter">
+      <div class="card">
+        <div class="card-content">
+          <div
+            v-if="validationErrors.length"
+            class="notification is-danger is-light"
+          >
+            <button @click="resetError()" class="delete"></button>
+            <div class="content">
+              Please resolve the following error(s) before proceeding.
+              <ul style="margin-top:0.3em; margin-left: 1em">
+                <li
+                  v-for="(error, index) in validationErrors"
+                  :key="`error-${index}`"
+                  v-html="error"
+                />
+              </ul>
+            </div>
+          </div>
+          <form>
+            <div class="field">
+              <label class="label">E-mail</label>
+              <div class="control">
+                <input
+                  v-model="email"
+                  class="input"
+                  type="text"
+                  autocomplete="email"
+                  placeholder="example@email.com"
+                />
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Password</label>
+              <div class="control">
+                <input
+                  v-model="password"
+                  class="input"
+                  type="password"
+                  autocomplete="current-password"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Repeat password</label>
+              <div class="control">
+                <input
+                  v-model="passwordRepeat"
+                  class="input"
+                  type="password"
+                  autocomplete="new-password"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+            <div class="field">
+              <p class="control">
+                <button @click.prevent="validate()" class="button is-success">
+                  Register
+                </button>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: null,
+      password: null,
+      passwordRepeat: null,
+      validationErrors: []
+    };
+  },
+  methods: {
+    resetError() {
+      this.validationErrors = [];
+    },
+    validate() {
+      this.resetError();
+      if (!this.email) {
+        this.validationErrors.push("<strong>E-mail</strong> cannot be empty.");
+      }
+      if (/.+@.+/.test(this.email) != true) {
+        this.validationErrors.push("<strong>E-mail</strong> must be valid.");
+      }
+      if (!this.password) {
+        this.validationErrors.push("<strong>Password</strong> cannot be empty");
+      }
+      if (/.{6,}/.test(this.password) != true) {
+        this.validationErrors.push(
+          "<strong>Password</strong> must be at least 6 characters long"
+        );
+      }
+      if (!(this.password === this.passwordRepeat)) {
+        this.validationErrors.push("<strong>Passwords</strong> did not match");
+      }
+      if (this.validationErrors.length <= 0) {
+        this.signUp();
+      }
+    },
+    signUp() {
+      console.log("sign up", this.email, this.password);
+    }
+  }
+};
+</script>
